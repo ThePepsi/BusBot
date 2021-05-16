069212bcedc1ec7da702feb0842abcc597a222d2
ThePepsi <timo.seitz96@outlook.com>
2021-05-16 19:41:37 +0200

    core.py and initGame as LayoutGuide

--- core.py
+++ core.py
@@ -1,2 +1,37 @@
-class core:
+import os
+import discord
+from enum import Enum
+
+class Core:
     
+    def __init__(self):
+        self.games = []
+
+    async def initGame(self, channel, guild, host):
+        self.games.append(Game(guild,host))
+        await channel.send('Hi')
+        
+
+
+class Game:
+
+    def __init__(self, guild, host):
+        #Guild
+        self.guild = guild
+
+        #Player
+        self.hostUser = host
+        self.playerList = []
+
+        #MessagesIDs 
+        self.msg_initMsg = None
+        self.dm_messages = []
+
+    
+
+
+
+class GameStatus(Enum):
+    INIT = 0
+    LOGIN = 1
+
