e4287b7e474ced8b8aa31d7e15494b75de429a8c
ThePepsi <timo.seitz96@outlook.com>
2021-05-16 22:27:22 +0200

    Pepsi not Happy, TODO Core and Game layout

--- core.py
+++ core.py
@@ -8,16 +8,45 @@ class Core:
         self.games = []
 
     async def initGame(self, channel, guild, host):
-        self.games.append(Game(guild,host))
-        await channel.send('Hi')
+        self.games.append(Game(guild,host,channel))
+        
+        txt = f'Willkommen bei BusBot. Jeder Mitspieler reagiert bitte mit einem Eigenen Emoji. Spieler {host.mention} das Spiel mit dem Emoji \U0001F504'
+        m = await channel.send(txt)
+        await m.add_reaction('\U0001F504') 
+    
+    async def joinGame(self, guild, player, emoji):
+        for game in self.games:
+            if game.guild == guild:
+                game.playerJoin(player, emoji)
+
+    async def startGame(self, user, guild):
+        game = next((game for game in self.games if game.guild == guild), None)
+        if game != None:
+            if game.hostUser != user:
+                return
+        
+        txt = f'Und los Geht es! Karten werden ausgeteilt! \n Wenn jeder seine Karten hat bitte \U0001F504 drücken. \n Mittspieler sind:\n'
+        for player in game.playerList:
+            txt = txt + f'{player[0]} => {player[1]}\n'
+
+        m = await game.channel.send(txt)
+        await m.add_reaction('\U0001F504') 
+                
+
+
+    def getGameState(self, guild):
+        return next((game.status for game in self.games if game.guild == guild), [GameStatus.NOT])
+        
+                
         
 
 
 class Game:
-
-    def __init__(self, guild, host):
+    def __init__(self, guild, host, channel):
+        self.status = GameStatus.INIT
         #Guild
         self.guild = guild
+        self.channel = channel
 
         #Player
         self.hostUser = host
@@ -27,11 +56,23 @@ class Game:
         self.msg_initMsg = None
         self.dm_messages = []
 
+    def playerJoin(self, player, emoji):
+        self.playerList.append((emoji,player))
+
+    def playerLeave(self, player):
+        for p in self.playerList:
+            if p[1] == player:
+                self.playerList.remove(p)
+                return True
+        return False
+
+
     
 
 
 
 class GameStatus(Enum):
-    INIT = 0
-    LOGIN = 1
+    NOT = 0
+    INIT = 1
+    LOGIN = 2
 
